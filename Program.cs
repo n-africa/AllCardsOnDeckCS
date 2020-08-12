823bc26c5be9d975cea22f85ca55c0f9be74f28f
Naima Africa Francis <naima.africa@gmail.com>
2020-08-12 16:42:53 -0400

    Update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var deck = new List<string>();
+            var suit = new List<string>() { "Spades", "Hearts", "Clubs", "Diamonds" };
+            var rank = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            for each(suit) { };
+            for each(rank){ };
+            deck.Add();
+
+            var n = deck.Count();
+
+            for (var index = 0; index < deck.Count; index++)
+
+
+
+        }
     }
-  }
 }
